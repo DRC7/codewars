e0b8dca5365fd0c02ae634064fd4d738b26dc408
Daniel <daniel.cuzco@gmail.com>
2024-03-27 22:03:28 -0400

    added first codewars

---
+++ 8kyu/array-practice.js
@@ -0,0 +1,7 @@
+// In this kata the function returns an array/list of numbers without its last element. The function is already written for you and the basic tests pass, but random tests fail. Your task is to figure out why and fix it.
+
+// My Solution
+
+function withoutMax(arr) {
+    return arr.slice(0, -1)
+}
