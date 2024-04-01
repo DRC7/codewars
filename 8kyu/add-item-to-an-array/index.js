bce57e9e8973a69b44818d9bf2c68d2f132ec5e1
Daniel <daniel.cuzco@gmail.com>
2024-04-01 13:20:02 -0400

    new 8kyu challenge

---
+++ 8kyu/add-item-to-an-array/index.js
@@ -0,0 +1,10 @@
+// Add the value "codewars" to the websites array.
+// After your code executes the websites array should == ["codewars"]
+
+// The websites array has already been defined for you using the following code:
+
+var websites = [];
+
+// My Solution
+
+websites.push('codewars')
