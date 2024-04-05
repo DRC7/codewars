04f2502e651c964641963a424957f01e2c9fe114
Daniel <daniel.cuzco@gmail.com>
2024-04-05 09:33:11 -0400

    string to numbers challenge

---
+++ 8kyu/sum-mixed-array/index.js
@@ -0,0 +1,10 @@
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+// Return your answer as a number.
+
+// My Solution
+
+function sumMix(x) {
+    return x.map(a => +a).reduce((a,b) => a + b)
+}
+
