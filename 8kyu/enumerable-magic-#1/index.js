c449eb32dac4db660598137699a7fb343afd717f
Daniel <daniel.cuzco@gmail.com>
2024-04-06 09:33:54 -0400

    using every method

---
+++ 8kyu/enumerable-magic-#1/index.js
@@ -0,0 +1,12 @@
+
+// Create a method all which takes two params:
+
+// a sequence
+// a function (function pointer in C)
+// and returns true if the function in the params returns true for every element in the sequence. Otherwise, it should return false. If the sequence is empty, it should return true, since technically nothing failed the test.
+
+// Upvoted Solution
+
+function all(arr, fun) {
+    return arr.every(fun)
+}
