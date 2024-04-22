1cb8dae0db8d2e37b1c8e4612624f379382096a4
Daniel <daniel.cuzco@gmail.com>
2024-04-22 08:11:54 -0400

    repeat method

---
+++ 8kyu/string-repeat/index.js
@@ -0,0 +1,8 @@
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+//My Solution
+
+function repeatStr(n, s) {
+    return s.repeat(n)
+}
+
