607488d83514c2236f5c1f5735da5037c35951b5
Daniel <daniel.cuzco@gmail.com>
2024-04-27 12:55:21 -0400

    template string

---
+++ 8kyu/template-string/index.js
@@ -0,0 +1,7 @@
+// Template Strings, this kata is mainly aimed at the new JS ES6 Update introducing Template Strings
+
+//My Solution
+
+const templeString = function(obj, feature) {
+    return `${obj} are ${feature}`
+}
