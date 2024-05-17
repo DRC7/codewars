31ed524cc0a9738ec5917331bedb38c8d8aa6d80
Daniel <daniel.cuzco@gmail.com>
2024-05-16 21:35:27 -0400

    split slice join

---
+++ 8kyu/remove-first-and-last-character-part-two/index.js
@@ -0,0 +1,10 @@
+// You are given a string containing a sequence of character sequences separated by commas.
+
+// Write a function which returns a new string containing the same character sequences except the first and the last ones but this time separated by spaces.
+
+// If the input string is empty or the removal of the first and last items would cause the resulting string to be empty, return an empty value (represented as a generic value NULL in the examples below).
+
+function array(arr) {
+    return arr.split(',').slice(1, -1).join(' ') || null;
+}
+
