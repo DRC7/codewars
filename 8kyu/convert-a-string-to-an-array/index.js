7f19934cf8167569efb8ee7bf821bc3a86ac8939
Daniel <daniel.cuzco@gmail.com>
2024-05-20 09:11:41 -0400

    split

---
+++ 8kyu/convert-a-string-to-an-array/index.js
@@ -0,0 +1,9 @@
+// Write a function to split a string and convert it into an array of words
+
+function stringToArray(str) {
+    return str.split('')
+}
+
+let words = 'i have to go to work soon'
+
+console.log(stringToArray(words))
