b22aac485cd3c6afa0c800e282490ebd8462b1eb
Daniel <daniel.cuzco@gmail.com>
2024-05-28 08:11:41 -0400

    math.pow

---
+++ 8kyu/alan-partridge-ii-apple-turnover/index.js
@@ -0,0 +1,17 @@
+// Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, else, return Help yourself to a honeycomb Yorkie for the glovebox.
+
+// My Solution
+
+function apple(x) {
+    if(x * x > 1000) {
+        return `It's hotter than the sun!!`
+    } else {
+        return `Help yourself to a honeycomb Yorkie for the glovebox.`
+    }
+}
+
+// Best Practice Solution
+
+function apple(x) {
+    return Math.pow(x, 2) > 1000 ? `It's hotter than the sun!!` : `Help yourself to a honeycomb Yorkie for the glovebox`
+}
