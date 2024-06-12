fd5e15765d92e3ac53fac93697f3bca7247f2f1f
Daniel <daniel.cuzco@gmail.com>
2024-06-12 09:09:21 -0400

    map and Number

---
+++ 7Kyu/convert-an-array-of-strings-to-array-of-numbers/app.js
@@ -0,0 +1,11 @@
+// You need to cast the whole array to the correct type.
+
+// Create the function that takes as a parameter a sequence of numbers represented as strings and outputs a sequence of numbers.
+
+// ie:["1", "2", "3"] to [1, 2, 3]
+
+// Note that you can receive floats as well.
+
+//My Solution
+
+const toNumberArray = stringArray => stringArray.map(Number)
